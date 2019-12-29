6ccb170a0e58969f132b7eece33b1bf7228162fb
Angry Wasp <angrywasp@live.com>
2019-12-29 15:18:42 +1100

    Application to verify a nerva node is online

--- Src/Nerva.NodeValidator/NetworkConnection.cs
+++ Src/Nerva.NodeValidator/NetworkConnection.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Sockets;
-using System.Threading.Tasks;
-using AngryWasp.Helpers;
+using System.Threading;
 using AngryWasp.Logger;
 using Nerva.Levin;
 using Nerva.Levin.Requests;
-using Nerva.Rpc.Daemon;
 
 namespace Nerva.NodeFinder
 {
@@ -31,13 +32,13 @@ namespace Nerva.NodeFinder
             }
         }
 
-        public static void VerifyPing(string host, Tuple<TcpClient, ThreadInfo> input)
+        public static bool VerifyPing(string host, TcpClient client)
         {
-            Log.Instance.Write($"{host}: Verifying");
+            Log.Instance.Write($"{host}: Validating");
 
             try
             {
-                NetworkStream ns = input.Item1.GetStream();
+                NetworkStream ns = client.GetStream();
 
                 object peerlist = null;
 
@@ -45,48 +46,39 @@ namespace Nerva.NodeFinder
                 Log.Instance.Write($"{host}: Sending handshake packet");
 
                 ns.Write(handshake, 0, handshake.Length);
-                ns.ReadTimeout = 5000;
+                ns.ReadTimeout = 30000;
 
                 Header header; Section section;
                 while (Read(host, ns, out header, out section))
                 {
                     if (header.Command == Constants.P2P_COMMAND_HANDSHAKE)
-                        peerlist = section.Entries["local_peerlist_new"];
-
-                    if (!ns.DataAvailable)
                     {
-                        Log.Instance.Write($"{host}: Network stream ended");
+                        peerlist = section.Entries["local_peerlist_new"];
+                        Log.Instance.Write("Retrieved peer list. Disconnecting");
                         break;
                     }
                 }
 
                 ns.Close();
-                input.Item1.Close();
+                client.Close();
 
                 if (peerlist != null)
-                {
-                    input.Item2.Found++;
-                    ProcessRemotePeerList(input.Item2, peerlist);
-                    Log.Instance.SetColor(ConsoleColor.Magenta);
-                    Log.Instance.Write($"{host}: Adding peer");
-                    Log.Instance.SetColor(ConsoleColor.White);
-
-    #pragma warning disable 4014
-                    new AddPeer(new AddPeerRequestData
-                    {
-                        Host = host
-                    }, null, null).RunAsync();
-    #pragma warning restore 4014
-                }
+                    return true;
             }
-            catch
+            catch (Exception ex)
             {
-                Log.Instance.Write($"{host}: Check failed");
+                Log.Instance.WriteNonFatalException(ex);
+                if (client != null)
+                    client.Close();
             }
+
+            return false;
         }
 
         private static bool Read(string host, NetworkStream ns, out Header header, out Section section)
         {
+            Thread.Sleep(25);
+
             header = null;
             section = null;
 
@@ -105,17 +97,35 @@ namespace Nerva.NodeFinder
 
             if (header == null)
             {
-                Log.Instance.Write($"{host}: Invalid header from remote node");
+                Log.Instance.Write(Log_Severity.Error, $"{host}: Invalid response from remote node");
                 return false;
             }
 
             offset = 0;
             byte[] buffer = new byte[header.Cb];
-            i = ns.Read(buffer, 0, buffer.Length);
 
-            if (i < buffer.Length)
+            int x = 0;
+            int y = 0;
+            do
             {
-                Log.Instance.Write($"{host}: Invalid response from remote node");
+                try
+                {
+                    y = ns.Read(buffer, x, buffer.Length);
+                    x += y;
+                    Thread.Sleep(25);
+                }
+                catch (Exception ex)
+                {
+                    Log.Instance.WriteNonFatalException(ex);
+                    return false;
+                }
+
+            } while (x < buffer.Length);
+
+            //read loop broke before we read as many bytes as the header says we need
+            if (x < buffer.Length)
+            {
+                Log.Instance.Write(Log_Severity.Error, $"{host}: Invalid response from remote node");
                 return false;
             }
 
@@ -130,43 +140,16 @@ namespace Nerva.NodeFinder
                     section = new SupportFlags().Read(header, buffer, ref offset);
                     break;
                 default:
-                    Log.Instance.Write(Log_Severity.Error, $"Command {header.Command} is not yet supported");
+                    if (header.Command >= 2001 && header.Command <= 2009)
+                        Log.Instance.Write($"Unsupported protocol notification {header.Command}");
+                    else
+                        Log.Instance.Write(Log_Severity.Warning, $"Command {header.Command} is not yet supported");
                     return false;
             }
 
             Log.Instance.Write($"{host}: Read data package {header.Command}");
 
             return true;
-        }
-
-        private static void ProcessRemotePeerList(ThreadInfo ti, object pl)
-        {
-            long tsNow = (long)DateTimeHelper.TimestampNow();
-            foreach (var obj in (Array)pl)
-            {
-                Section sec = (Section)obj;
-                Section adr = (Section)sec.Entries["adr"];
-                uint ipInt = (uint)((Section)adr.Entries["addr"]).Entries["m_ip"];
-                string ip = Helpers.ToIP(ipInt);
-                ushort rpcPort = (ushort)sec.Entries["rpc_port"];
-                long lastSeen = (long)sec.Entries["last_seen"];
-
-                if (tsNow - lastSeen > (60 * 60 * 8))
-                    continue; //skip stale peers
-
-                if (rpcPort != 0)
-                    Log.Instance.Write($"Found open RPC port @ {ip}:{rpcPort}");
-                
-                TcpClient client;
-                
-                if (NetworkConnection.Ping(ip, out client))
-                {
-                    Log.Instance.Write($"{ip}: Ping success");
-                    WorkQueue.Push(ip, client, ti);
-                }
-                else
-                    Log.Instance.Write($"{ip}: Ping failed");
-            }
         }
     }
 }
---
+++ Src/Nerva.NodeValidator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using AngryWasp.Logger;
+using AngryWasp.Helpers;
+
+namespace Nerva.NodeFinder
+{
+    public static class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            CommandLineParser clp = CommandLineParser.Parse(args);
+            Log.CreateInstance(true);
+
+            if (clp["--host"] == null)
+                Log.Instance.Write(Log_Severity.Fatal, "Need a host");
+
+            string host = clp["--host"].Value;
+            TcpClient client;
+
+            if (!NetworkConnection.Ping(host, out client))
+            {
+                Log.Instance.Write(Log_Severity.Error, "Validation failed. No ping");
+                return;
+            }
+
+            if (!NetworkConnection.VerifyPing(host, client))
+            {
+                Log.Instance.Write(Log_Severity.Error, "Validation failed. Node not detected");
+                return;
+            }
+
+            Log.Instance.Write("Verified OK");
+        }
+    }
+}
